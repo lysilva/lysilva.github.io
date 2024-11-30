a8bf44a1c5f29f9cedc63702e2c206fd467be1d1
lysilva <slivasousalivia050@gmail.com>
2024-11-30 15:29:03 -0300

    upando arquivos flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Psicologia',
+    'O que é empatia?',
+    'Empatia é a capacidade de compreender e compartilhar os sentimentos de outra pessoa.'
+)
+
+criaCartao(
+        'Economia',
+        'O que é investimento?',
+        'Investimento é o ato de aplicar dinheiro em algo com a expectativa de obter retorno ou lucro no futuro.'
+)
+
+criaCartao(
+    'Música',
+    'Por que a música é importante?',
+    'A música é importante porque promove expressão cultural, alivia o estresse, melhora a saúde mental e conecta pessoas por meio de emoções e histórias compartilhadas.'
+)
+
+criaCartao(
+    'Saúde e Bem-Estar',
+    'Por que a hidratação é importante?',
+    'A hidratação é essencial para o funcionamento do corpo, regulando temperatura, transportando nutrientes e mantendo o equilíbrio celular.'
+)
